e0793a5084761e5193c89775a38fc1b3184608f7
felipem1145 <57733624+felipem1145@users.noreply.github.com>
2019-12-11 20:04:50 -0500

    Update db-schema.sql

--- db-schema.sql
+++ db-schema.sql
@@ -1,2 +1,20 @@
 CREATE SCHEMA Employee
 GO
+
+
+
+
+
+--EMPLOYEE PROJECT
+
+CREATE TABLE Employee.EmployeeProject
+
+(
+	EmployeeId INT IDENTITY(1,1),  
+	ProjectId  INT,
+	ExpectedFinishDate DATE,
+	HoursWorked DECIMAL(4,1),
+	ExtraHours DECIMAL(3,1)
+);
+
+GO

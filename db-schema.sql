a23fe89111d3b7fc7d767c04d9689c14718ddf5c
felipem1145 <57733624+felipem1145@users.noreply.github.com>
2019-12-16 20:39:46 -0500

    Update db-schema.sql

--- db-schema.sql
+++ db-schema.sql
@@ -1,11 +1,50 @@
-CREATE SCHEMA Employee
+CREATE DATABASE SALARY_MANAGEMENT
+GO
+USE SALARY_MANAGEMENT
 GO
 
 
+CREATE SCHEMA Employee
+GO
 
+CREATE SCHEMA Company
+GO
 
 
 --EMPLOYEE PROJECT
+DROP TABLE Employee.Employee
+CREATE TABLE Employee.Employee
+(
+	EmployeeID INT PRIMARY KEY,
+	EmployeeFirstName varchar(30),
+	EmployeeLastName varchar(30),
+	RoleId INT,
+	Department VARCHAR(30),
+	EmployeeSalary DECIMAL(8,4),
+	EmployeeSavings DECIMAL(12,4),
+	SalaryCTC DECIMAL(8,4),
+	PFDeduction DECIMAL(8,4),
+	SalaryIncrement DECIMAL(5,4),
+	NetSalary DECIMAL(8,4),
+	OtherDeduction DECIMAL(5,4)
+
+);
+GO
+
+SELECT *
+FROM Employee.Employee
+GO
+
+CREATE TABLE Employee.EmployeeRole
+(
+	EmployeeID INT,
+	RoleID INT,
+	Salary DECIMAL(8,3), 
+	Startdate DATE,
+	Finishdate DATE,
+	
+);
+GO
 
 CREATE TABLE Employee.EmployeeProject
 
@@ -16,5 +55,22 @@ CREATE TABLE Employee.EmployeeProject
 	HoursWorked DECIMAL(4,1),
 	ExtraHours DECIMAL(3,1)
 );
-
 GO
+
+CREATE TABLE Company.CompanyRoles
+(
+	RoleID INT PRIMARY KEY,
+	Department VARCHAR(30),
+	Position VARCHAR(30),
+	ExpectedCTC DECIMAL(8,3)
+);
+GO
+
+CREATE TABLE Company.Projects
+(
+	ProjectID INT PRIMARY KEY,
+	ProjectName VARCHAR(40),
+	ProjectManager VARCHAR(60)
+);
+GO
+

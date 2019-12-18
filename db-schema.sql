b055f9b8a551ed368a24bfc246dd0cc364fc056a
felipem1145 <57733624+felipem1145@users.noreply.github.com>
2019-12-17 20:36:18 -0500

    DataInserted

--- db-schema.sql
+++ db-schema.sql
@@ -1,7 +1,12 @@
-CREATE DATABASE SALARY_MANAGEMENT
-GO
 USE SALARY_MANAGEMENT
 GO
+
+DROP DATABASE IF EXISTS SALARY_MANAGEMENT;
+GO
+
+CREATE DATABASE SALARY_MANAGEMENT
+GO
+
 
 
 CREATE SCHEMA Employee
@@ -26,48 +31,55 @@ SELECT *
 FROM Employee.Employee
 GO
 
+DROP TABLE Employee.EmployeeRole
 CREATE TABLE Employee.EmployeeRole
 (
-	EmployeeID INT,
-	RoleID INT,
-	Salary DECIMAL(8,3), 
+	EmployeeID INT CONSTRAINT FKEmployeeID FOREIGN KEY REFERENCES Employee.Employee(EmployeeID),
+	RoleID INT CONSTRAINT FK_RoleID FOREIGN KEY REFERENCES Company.CompanyRoles (RoleID),
+	Salary DECIMAL(15,3), 
 	Startdate DATE,
 	Finishdate DATE,
-	EmployeeSavings DECIMAL(12,4),
-	SalaryCTC DECIMAL(8,4),
-	PFDeduction DECIMAL(8,4),
-	SalaryIncrement DECIMAL(5,4),
-	NetSalary DECIMAL(8,4),
-	OtherDeduction DECIMAL(5,4)
+	EmployeeSavings DECIMAL(15,4),
+	SalaryCTC DECIMAL(15,4),
+	PFDeduction DECIMAL(15,4),
+	SalaryIncrement DECIMAL(12,4),
+	NetSalary DECIMAL(15,4),
+	OtherDeduction DECIMAL(12,4)
 	
 );
 GO
+SELECT * FROM Employee.EmployeeRole
 
 CREATE TABLE Employee.EmployeeProject
 
 (
-	EmployeeId INT IDENTITY(1,1),  
-	ProjectId  INT,
+	EmployeeId INT CONSTRAINT FK_EmployeeId FOREIGN KEY REFERENCES Employee.Employee(EmployeeID),  
+	ProjectId  INT CONSTRAINT FK_ProjectId FOREIGN KEY REFERENCES Company.Projects(ProjectID),
 	ExpectedFinishDate DATE,
 	HoursWorked DECIMAL(4,1),
-	ExtraHours DECIMAL(3,1)
+	ExtraHours DECIMAL(4,1)
 );
 GO
+
 
 CREATE TABLE Company.CompanyRoles
 (
 	RoleID INT PRIMARY KEY,
-	Department VARCHAR(30),
-	Position VARCHAR(30),
-	ExpectedCTC DECIMAL(8,3)
+	Department VARCHAR(50), 
+	Position VARCHAR(50),
+	ExpectedCTC DECIMAL(12,3)
 );
 GO
+
+
 
 CREATE TABLE Company.Projects
 (
 	ProjectID INT PRIMARY KEY,
-	ProjectName VARCHAR(40),
+	ProjectName VARCHAR(100),
 	ProjectManager VARCHAR(60)
 );
+
 GO
+
 
